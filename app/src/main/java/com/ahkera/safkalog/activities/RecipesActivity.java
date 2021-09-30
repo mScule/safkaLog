b4f4547c5ba720e644e3c5952ae980ae2f71c45c
Konstakoira <konsta.perttila@hotmail.com>
2021-09-30 23:09:40 +0300

    SafkaLog 0.1.0 activity_recipes.xml layout & String stuff

--- app/src/main/java/com/ahkera/safkalog/activities/RecipesActivity.java
+++ app/src/main/java/com/ahkera/safkalog/activities/RecipesActivity.java
@@ -9,6 +9,11 @@ import com.ahkera.safkalog.R;
 public class RecipesActivity extends AppCompatActivity {
 
 
+    public static final String
+            GRAMS = "com.ahkera.safkalog.activities.GramActivity.GRAM_ACTIVITY";
+
+    private int grams;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
